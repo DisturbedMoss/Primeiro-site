5043948e0709a3913f0537ff82cc7fc2c5b7cff9
Vitor Hugo <disturbedmoss35@gmail.com>
2021-10-28 08:40:56 -0300

    Primeiro site

---
+++ scripthogwarts_v1.js
@@ -0,0 +1,12 @@
+function clickson(){
+    alert('Você clicou em Sonserina.')
+}
+function clickgri(){
+    alert('Você clicou em Grifinória.')
+}
+function clickluf(){
+    alert('Você clicou em Lufa-Lufa.')
+}
+function clickcor(){
+    alert('Você clicou em Corvinal.')
+}
